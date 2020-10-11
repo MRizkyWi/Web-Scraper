e084c537bed1e0973cc10fbb07234a2d08f356ee
cerohawke88 <vickysultan08@gmail.com>
2020-10-11 19:36:22 +0700

    restructure json, add newest articles array

--- solution.js
+++ solution.js
@@ -1,6 +1,6 @@
 /** web scraper for cermati article
-  * made by: M. Rizky Widyayulianto
-*/
+ * made by: M. Rizky Widyayulianto
+ */
 
 const rp = require('request-promise')
 const cheerio = require('cheerio')
@@ -11,82 +11,104 @@ const cermati = 'https://www.cermati.com'
 
 var list = []
 var listArticles = []
+// var list2 = []
+var listNewestArticles = []
 
 rp(url)
-    .then(function(html){
-        // load all the article link
-        var $ = cheerio.load(html)
-        $('div.article-list-item').map((i, element) => {
-            var link = $(element).find('a').attr('href')
+.then(function(html){
+    // load all the article link
+    var $ = cheerio.load(html)
+    $('div.article-list-item').map((i, element) => {
+        var link = $(element).find('a').attr('href')
+        
+        // add https
+        link = cermati.concat(link)
+        
+        list.push(link)
+    })
+
+    $('h4:contains("Artikel Terbaru")').next().find('li').map((i, newArt) => {
+        var backlink = $(newArt).find('a').attr('href')
+        var fulllink = cermati.concat(backlink)
+        var titleArticle = $(newArt).find('h5').text().trim()
+        var newArtCategory = $(newArt).find('span.item-category').text().trim()
+        var newArtDate = $(newArt).find('span.item-publish-date').text().trim()
+        var newestArticle = {
+            "url": fulllink,
+            "title": titleArticle,
+            "category": newArtCategory,
+            "date": newArtDate.replace('•\n                      ',''),
+        }
+        listNewestArticles.push(newestArticle)
+    })
+    
+    // get all detail for each article
+    list.forEach(function(element, i){
+        rp(element)
+        .then(function(linkArticles){
+            var $ = cheerio.load(linkArticles)
             
-            // add https
-            link = cermati.concat(link)
+            // article title
+            var title = $('h1.post-title').text().trim()
+            
+            // article author
+            var author = $('span.author-name').text().trim()
+            
+            // article category
+            var category = $('div.post-category>a').text().trim()
+            
+            // article posting date
+            var date = $('span.post-date').find('span').text().trim()
+            
+            
+            // related articles
+            var relatedArticles = []
+            $('h4:contains("Artikel Terkait")').next().find('li').map((i, relArt) => {
+                var backlink = $(relArt).find('a').attr('href')
+                var fulllink = cermati.concat(backlink)
+                var titleArticle = $(relArt).find('h5').text().trim()
+                var relArtCategory = $(relArt).find('span.item-category').text().trim()
+                var relArtDate = $(relArt).find('span.item-publish-date').text().trim()
+                var relatedArticle = {
+                    "url": fulllink,
+                    "title": titleArticle,
+                    "category": relArtCategory,
+                    "date": relArtDate.replace('•\n                      ',''),
+                }
+                relatedArticles.push(relatedArticle)
+            })
+            
+            // make JSON Object
+            var article = {
+                "url": element,
+                "title": title,
+                "author": author,
+                "category": category,
+                "postingDate": date,
+                "relatedArticles": relatedArticles
+            }
+            
+            listArticles.push(article)
 
-            list.push(link)
         })
-
-        // get all detail for each article
-        list.forEach(function(element, i){
-            rp(element)
-                .then(function(linkArticles){
-                    var $ = cheerio.load(linkArticles)
-
-                    // article title
-                    var title = $('h1.post-title').text().trim()
-
-                    // article author
-                    var author = $('span.author-name').text().trim()
-
-                    // article category
-                    var category = $('div.post-category>a').text().trim()
-
-                    // article posting date
-                    var date = $('span.post-date').find('span').text().trim()
-
-                    // related articles
-                    var relatedArticles = []
-                    $('h4:contains("Artikel Terkait")').next().find('li').map((i, relArt) => {
-                        var backlink = $(relArt).find('a').attr('href')
-                        var fulllink = cermati.concat(backlink)
-                        var titleArticle = $(relArt).find('h5').text().trim()
-                        var relArtCategory = $(relArt).find('span.item-category').text().trim()
-                        var relArtDate = $(relArt).find('span.item-publish-date').text().trim()
-                        var relatedArticle = {
-                            "url": fulllink,
-                            "title": titleArticle,
-                            "category": relArtCategory,
-                            "date": relArtDate.replace('•\n                      ',''),
-                        }
-                        relatedArticles.push(relatedArticle)
-                    })
-
-                    // make JSON Object
-                    var article = {
-                        "url": element,
-                        "title": title,
-                        "author": author,
-                        "category": category,
-                        "postingDate": date,
-                        "relatedArticles": relatedArticles
-                    }
-
-                    listArticles.push(article)
-                })
-                .catch(function(error) {
-                    console.log(error)
-                })
-                .finally(function() {
-                    // check if it's on last article
-                    if (i === list.length - 1){
-                        var articles = {
-                            "articles": listArticles
-                        }
-                        var jsonArticle = JSON.stringify(articles);
-                        fs.writeFileSync("solution.json", jsonArticle)
-                    }
-                })
+        .catch(function(error) {
+            console.log(error)
+        })
+        .finally(function() {
+            // check if it's on last article
+            if (i === list.length - 1){
+                var articles = {
+                    "articles": {
+                        "main": listArticles,
+                        "newest_articles": listNewestArticles,
+                    },
+                }
+                var jsonArticle = JSON.stringify(articles);
+                fs.writeFileSync("solution.json", jsonArticle)
+            }
         })
     })
-    .catch(function(error){
-        console.log(error)
-    })
+})
+.catch(function(error){
+    console.log(error)
+})
